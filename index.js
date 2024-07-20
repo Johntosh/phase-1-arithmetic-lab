bd99e16b514a971cb2dcf462ddd98a7882ef0c3f
Johntosh <kariukijohngitonga@gmail.com>
2024-07-21 01:53:21 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+// Variables for multiplication
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+console.log("multiply:", multiply);
 
-// Write your code here
+// Variable for random integer
+var random = Math.floor(Math.random() * 100) + 1;
+console.log("random:", random);
+
+// Variables for modulus
+var num3 = 44;
+var num4 = 10;
+var mod = num3 % num4;
+console.log("mod:", mod);
+
+// Variable for maximum value
+var max = Math.max(10, 15, 20, 5, 12); 
+console.log("max:", max);
+
+
+
+
+
